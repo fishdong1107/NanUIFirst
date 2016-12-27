d4449b65317d7f56d51e2c2ee603e6160061aa16
fishdong1107 <Administrator@PC-201501122045>
2016-12-27 20:56:56 +0800

    添加项目文件。

---
+++ NanUIFirst/MainForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NetDimension.NanUI;
+
+namespace NanUIFirst
+{
+    public partial class MainForm : HtmlUIForm
+    {
+        public MainForm() : base("embedded://www/about.html")
+        {
+            InitializeComponent();
+        }
+    }
+}
